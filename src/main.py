536bd260b8d9a75a7aa386de47896596aef00be3
Marvin Lutz <marvin.lutz@fh-bielefeld.de>
2016-08-01 12:44:02 +0200

    no message

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from tinkerforge.ip_connection import IPConnection
 
 from bindings.brick_red import RED
 
+# HOST = "192.168.0.67"
 HOST = "localhost"
 PORT = 4223
 RED_UID = "3dfBkF"
+#RED_UID = "37GByw"
 
 counter = 0
 
@@ -22,30 +24,37 @@ def tv_init():
     print "Init TinkerVision"
     global gray, face, snap, scene
     # gray = red.vision_module_start("grayfilter")
-    face = red.vision_module_start("cascadeclassifier")
+    face = red.vision_module_start("downscale")
+    # red.vision_numerical_parameter_set(face.id, "max-hue", 50) #max h    180
+    # red.vision_numerical_parameter_set(face.id, "min-hue", 0) #min h
+    # red.vision_numerical_parameter_set(face.id, "max-saturation", 255) #max s    255
+    # red.vision_numerical_parameter_set(face.id, "min-saturation", 200) #min s
+    # red.vision_numerical_parameter_set(face.id, "max-value", 255) #max v
+    # red.vision_numerical_parameter_set(face.id, "min-value", 50) #min v
+
     snap = red.vision_module_start("snapshot")
     print gray, face, snap
 
-    scene = red.vision_scene_start(gray.id)
-    print scene
-    print "Scene Face add: " + str(red.vision_scene_add(scene.scene_id, face.id))
-    print "Scene Snap add: " + str(red.vision_scene_add(scene.scene_id, snap.id))
+    # scene = red.vision_scene_start(face.id)
+    # # print "Scene Face add: " + str(red.vision_scene_add(scene.scene_id, face.id))
+    # print "Scene Snap add: " + str(red.vision_scene_add(scene.scene_id, snap.id))
 
 
 def end_demo():
     print "Ending Demo"
     #stop tv, destroy tv, destroy ipcon
     print "TinkerVisione RM Modules: " + str(red.vision_remove_all_modules())
-    print "TinkerVisione RM Scene  : " + str(red.vision_scene_remove(scene.scene_id))
+    # print "TinkerVisione RM Scene  : " + str(red.vision_scene_remove(scene.scene_id))
     ipcon.disconnect()
 
 
 def vision_callback(id, x, y, w, h, msg):
-    if id != snap.id:
-        return
+    # print "CB"
+    # if id != snap.id:
+    #     return
 
     global counter, end
-    if counter == 5:
+    if counter == 10:
         end = True
         return
     print id, x, y, w, h, msg
@@ -62,6 +71,7 @@ if __name__ == "__main__":
     tv_init()
     red.register_callback(RED.CALLBACK_VISION_MODULE, vision_callback)
 
+    print "Loop"
     while not end:
         sleep(1)
 
